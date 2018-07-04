f2c6f24bcd01efc878389877fa24844412e34d8b
Andrew Artajos <andrew.artajos@icloud.com>
2018-07-04 13:14:45 +1000

    Finished implementation and testing

--- Match.js
+++ Match.js
@@ -1,11 +1,42 @@
 'use strict';
 
+class Player {
+
+  constructor(name) {
+    this.name = name;
+    this.pointsWon = 0;
+    this.gamesWon = 0;
+  }
+
+  pointWon(name) {
+    if (this.name === name) {
+      this.pointsWon++;
+    }
+  }
+
+  resetPoints() {
+    this.pointsWon = 0;
+  }
+
+  gameWon() {
+    this.gamesWon++;
+  }
+
+  show() {
+    console.log(`${this.name} Games won ${this.gamesWon}, Points won ${this.pointsWon}`);
+  }
+}
+
 export default class Match {
 
   constructor(player1, player2) {
-    this.scoreBoard = {};
-    this.POINT = {
-      0: "LOVE",
+    this.playerOne = new Player(player1);
+    this.playerTwo = new Player(player2);
+    this.isTieBreak = false;
+    this.scoreBoard = [];
+
+    this.TENNIS_POINT = {
+      0: "0",
       1: "15",
       2: "30",
       3: "40",
@@ -15,10 +46,108 @@ export default class Match {
   }
 
   /**
+   * Returns the players sorted by the pointsWon in ascending order
+   */
+  getPlayersByPoints() {
+    return [this.playerOne, this.playerTwo].sort((a, b) => a.pointsWon - b.pointsWon);
+  }
+
+  /**
+   * Returns the players sorted by the gamesWon in ascending order
+   */
+  getPlayersByGames() {
+    return [this.playerOne, this.playerTwo].sort((a, b) => a.gamesWon - b.gamesWon);
+  }
+
+  /**
+   * Returns a string representation of the score in tennis lingo.
+   */
+  getTennisPoint() {
+    let playerOnePoints = this.playerOne.pointsWon;
+    let playerTwoPoints = this.playerTwo.pointsWon;
+
+    if (this.isTieBreak) {
+      return `, ${playerOnePoints}-${playerTwoPoints}`;
+    }
+
+    if (playerOnePoints >= 3 && playerOnePoints === playerTwoPoints) {
+      return `, Deuce`;
+    }
+
+    if (playerOnePoints >= 3 && playerTwoPoints >= 3) {
+      let players = this.getPlayersByPoints();
+      let leading = players.pop();
+      return `, Advantage ${leading.name}`;
+    }
+
+    return `, ${this.TENNIS_POINT[playerOnePoints]}-${this.TENNIS_POINT[playerTwoPoints]}`;
+  }
+
+  /**
    * Displays the current score of the tennis match
    */
   score() {
+    let player1Points = this.playerOne.pointsWon;
+    let player2Points = this.playerTwo.pointsWon;
+    let player1Games = this.playerOne.gamesWon;
+    let player2Games = this.playerTwo.gamesWon;
 
+    let showPlayerPoints = !((player1Points === 0) && (player2Points === 0));
+
+    let output = `${player1Games}-${player2Games}`;
+    if (showPlayerPoints) {
+      output += this.getTennisPoint(this.player);
+    }
+    // this.playerOne.show();
+    // this.playerTwo.show();
+    return output;
+  }
+
+  /**
+   * Updates the games stat applying the tennis rules.
+   */
+  updateGamesWon() {
+
+    let players = this.getPlayersByPoints();
+    let leading = players.pop();
+    let trailing = players.pop();
+
+    if (this.isTieBreak) {
+      if (leading.pointsWon >= 7 && leading.pointsWon >= trailing.pointsWon + 2) {
+        leading.gameWon();
+        leading.resetPoints();
+        leading.resetPoints();
+      }
+    } else {
+      if (leading.pointsWon >= 4 && leading.pointsWon >= trailing.pointsWon + 2) {
+        leading.gameWon();
+        leading.resetPoints();
+        trailing.resetPoints();
+      }
+    }
+
+    players = this.getPlayersByGames();
+    leading = players.pop();
+    trailing = players.pop();
+
+    if (leading.gamesWon >= 6) {
+
+      if (this.isTieBreak) {
+        if (leading.gamesWon == 7 && trailing.gamesWon == 6) {
+          console.log(`Tie break won by ${leading.name}`);
+          this.isTieBreak = false;
+          return;
+        }
+      } else if (leading.gamesWon >= trailing.gamesWon + 2) {
+        console.log(`${leading.name} wins the set! ${leading.gamesWon}-${trailing.gamesWon}`);
+        return;
+      }
+    }
+
+    if (leading.gamesWon == 6 && leading.gamesWon === trailing.gamesWon) {
+      this.isTieBreak = true;
+      console.log(`Tie break is on!`);
+    }
   }
 
   /**
@@ -26,6 +155,10 @@ export default class Match {
    * @param {string} player - who scored the point
    */
   pointWonBy(player) {
+    // record the score
+    this.playerOne.pointWon(player);
+    this.playerTwo.pointWon(player);
 
+    this.updateGamesWon();
   }
 }
--- spec/Match.spec.js
+++ spec/Match.spec.js
@@ -2,9 +2,120 @@ import Match from '../Match';
 
 describe("Match tests", () => {
 
-  it("should create a match object", () => {
-    let match = new Match("p1", "p2");
-    expect(match.currentSet).toBe(1);
+  let match = null;
+  let result = null;
+  let winGames = ((match, player, games) => {
+    for (let i=0; i < games; i++) {
+      match.pointWonBy(player);
+      match.pointWonBy(player);
+      match.pointWonBy(player);
+      match.pointWonBy(player);
+    }
+  });
+
+  let winTieBreak = ((match, player) => {
+    // Needs 7 points to win the tie break
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+    match.pointWonBy(player);
+  });
+
+  beforeEach(() => {
+    match = new Match("p1", "p2");
+  });
+
+  it("should not display the points", () => {
+    result = match.score();
+    expect(result.includes(',')).toBe(false);
+  });
+
+  it("should display game and points score", () => {
+    match.pointWonBy("p1");
+    result = match.score();
+    expect(result.includes(',')).toBe(true);
+    expect(result).toBe("0-0, 15-0");
+    match.pointWonBy("p1");
+    result = match.score();
+    expect(result).toBe("0-0, 30-0");
+    match.pointWonBy("p1");
+    result = match.score();
+    expect(result).toBe("0-0, 40-0");
+    match.pointWonBy("p1");
+    result = match.score();
+    expect(result.includes(',')).toBe(false);
+    expect(result).toBe("1-0");
+  });
+
+  it("should display deuce when both players have atleast 3 points and have the same number of points", () => {
+    match.pointWonBy("p1");
+    match.pointWonBy("p1");
+    match.pointWonBy("p1");
+
+    match.pointWonBy("p2");
+    match.pointWonBy("p2");
+    match.pointWonBy("p2");
+
+    result = match.score();
+    expect(result.includes(',')).toBe(true);
+    expect(result).toBe("0-0, Deuce");
+
+  });
+
+  it("should display advantage for the player who scored at least 3 points and ahead of the other player", () => {
+    match.pointWonBy("p1");
+    match.pointWonBy("p1");
+    match.pointWonBy("p1");
+
+    match.pointWonBy("p2");
+    match.pointWonBy("p2");
+    match.pointWonBy("p2");
+
+    result = match.score();
+    expect(result.includes(',')).toBe(true);
+    expect(result).toBe("0-0, Deuce");
+
+    match.pointWonBy("p2");
+    result = match.score();
+    expect(result).toBe("0-0, Advantage p2");
+  });
+
+  it("should detect a player has won the set", () => {
+    winGames(match, "p1", 3);
+    winGames(match, "p2", 6);
+
+    result = match.score();
+    expect(result).toBe("3-6");
+  });
+
+  it("should detect a tie break", () => {
+    winGames(match, "p1", 5);
+    winGames(match, "p2", 5);
+    winGames(match, "p1", 1);
+    winGames(match, "p2", 1);
+
+    result = match.score();
+    expect(result).toBe("6-6");
+    expect(match.isTieBreak).toBe(true);
+  });
+
+  it("should simulate a set won in a tie break", () => {
+    winGames(match, "p1", 5);
+    winGames(match, "p2", 5);
+    winGames(match, "p1", 1);
+    winGames(match, "p2", 1);
+
+    result = match.score();
+    expect(result).toBe("6-6");
+    expect(match.isTieBreak).toBe(true);
+    winTieBreak(match, "p2");
+    result = match.score();
+    expect(result).toBe("6-7");
+    expect(match.isTieBreak).toBe(false);
   });
 
 });
+
